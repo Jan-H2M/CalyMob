07e334e7358b210535c10dc9c8aa95fe4211c7bb
Jan Andriessens <jan@h2m.ai>
2026-04-12 15:47:15 +0200

    fix(android): remove duplicate MainActivity.kt from old package names

--- android/app/src/main/kotlin/club/caly/calymob/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package club.caly.calymob
-
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-class MainActivity: FlutterFragmentActivity() {
-}
--- android/app/src/main/kotlin/com/example/calycompta_mobile/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.calycompta_mobile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
